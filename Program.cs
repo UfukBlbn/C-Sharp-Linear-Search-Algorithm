0ba657363b9487fba24579ccbf0bc5d7b09e078e
UfukBlbn <71414017+UfukBlbn@users.noreply.github.com>
2022-05-21 04:21:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        
+        static Boolean LinearSearch(int [] inputArray,int n)
+        {
+            foreach (var current in inputArray)
+            {
+                if (current==n)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int [ ] inputArray = {1,2,3,4,5,6,7,8,9};
+           Console.WriteLine(LinearSearch(inputArray,3));
+
+           Array.Find(inputArray,i=>i==3);
+
+           int [] items = Array.FindAll(inputArray,i=>i>=3);
+           Array.ForEach(items,Console.WriteLine);
+
+        }
+    }
+}
